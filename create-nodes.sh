bde0d2764155a7b5692999857585f2b7b25ed8ad
Levi Durfee <levi.durfee@gmail.com>
2017-12-19 21:38:40 -0500

    enable private networking

--- create-nodes.sh
+++ create-nodes.sh
@@ -5,6 +5,7 @@ docker-machine create --driver digitalocean \
 --digitalocean-size 1gb \
 --digitalocean-ssh-key-fingerprint a9:a9:3c:0b:4a:34:f1:92:62:b0:54:80:67:32:19:92 \
 --digitalocean-tags docker,dev \
+--digitalocean-private-networking \
 --digitalocean-access-token $DOTOKEN node-1 &
 
 docker-machine create --driver digitalocean \
@@ -12,6 +13,7 @@ docker-machine create --driver digitalocean \
 --digitalocean-size 1gb \
 --digitalocean-ssh-key-fingerprint a9:a9:3c:0b:4a:34:f1:92:62:b0:54:80:67:32:19:92 \
 --digitalocean-tags docker,dev \
+--digitalocean-private-networking \
 --digitalocean-access-token $DOTOKEN node-2 &
 
 docker-machine create --driver digitalocean \
@@ -19,4 +21,5 @@ docker-machine create --driver digitalocean \
 --digitalocean-size 1gb \
 --digitalocean-ssh-key-fingerprint a9:a9:3c:0b:4a:34:f1:92:62:b0:54:80:67:32:19:92 \
 --digitalocean-tags docker,dev \
+--digitalocean-private-networking \
 --digitalocean-access-token $DOTOKEN node-3 &
